ca931d34ec46f80c53023a8bb54f21a4b05e2f0e
SusanaChen <grupochen@hotmail.com>
2022-03-09 18:43:27 +0100

    empezando proyecto videoclub

---
+++ movies/movies_model.js
@@ -0,0 +1,5 @@
+const movies = [
+    {"id":10,"title":"Scent of a woman","genre":"drama","leadActor":"AlPacino",},
+    {"id":12,"title":"Red Alert","genre":"comedy","leadActor":"TheRock",},
+    {"id":20,"title":"Hotel Transilvania","genre":"kids","leadActor":"Adam Sandler",},
+]
