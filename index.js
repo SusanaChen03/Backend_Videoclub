48bc7c615e8b037fe667341ace7987efae84d039
SusanaChen <grupochen@hotmail.com>
2022-03-09 18:59:20 +0100

    resolucion de errores, vinculacion de archivos

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-const router = require('./movies/movies_model');
+const router = require('./movies/movies_router');
 
 app.listen(port,()=>console.log('Servidor levantado en puerto 8080'));
 
