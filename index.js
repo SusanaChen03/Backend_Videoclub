fb7827ba3639466a9e9dc8ebaa14e12f4c990d94
SusanaChen <grupochen@hotmail.com>
2022-03-18 11:34:08 +0100

    cambios para heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';     //import expressApi from express
 import routerUsers from './users/user_router.js';
 import routerMovies from './movies/movies_router.js';
 import  connection  from './config/database.js';
-const port = 8020;
+
 
 const app = express();
 
@@ -14,7 +14,13 @@ app.use(express.json());
 app.use('/',routerMovies);
 app.use('/',routerUsers);
 
-app.listen(port,()=>console.log('Servidor levantado en puerto 8020'));
+app.set("port",process.env.PORT || 8020);
+
+app.listen(app.get("port"),()=>{
+    console.log('Servidor levantado' );
+});
+
+//app.listen(8020,()=>console.log('Servidor levantado en puerto 8020'));
 
 
 
